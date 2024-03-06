a55dfc059576f8d9a61665500fc7bd1e1ff5d1f6
kokasmark <mark.kokas04@gmail.com>
2024-03-06 14:08:48 +0100

    Diet Page ground work.

--- FlexifyMobile/DietPage.xaml.cs
+++ FlexifyMobile/DietPage.xaml.cs
@@ -1,22 +1,57 @@
 using Microsoft.Maui.Controls;
 using SkiaSharp;
 using SkiaSharp.Views.Maui;
+using System.Text.Json;
+
 namespace FlexifyMobile;
 
 public partial class DietPage : ContentPage
 {
     FlexifyDatabase database;
     string token = "";
+    DietResult dietResult;
     public DietPage()
 	{
 		InitializeComponent();
         database = new FlexifyDatabase();
         List<User> users = database.GetItemsAsync();
         token = users[users.Count - 1].token;
+
+        getDiet(token, $"{DateTime.Now.Year}-{DateTime.Now.Month:D2}-{3:D2}");
     }
 
     private void Home_Clicked(object sender, EventArgs e)
     {
         Shell.Current.GoToAsync($"//HomePage?Token={token}", false);
+    }
+    void InatializeDiet()
+    {
+
+    }
+    async void getDiet(string token, string d)
+    {
+        var client = new HttpClient();
+
+        var jsonBody = $"{{\"date\": \"{d}\", \"location\": \"{"mobile"}\" }}";
+        var content = new StringContent(jsonBody, null, "application/json");
+
+        var request = new HttpRequestMessage(HttpMethod.Post, $"http://{Constants.hostname}:3001/api/diet");
+        request.Headers.Add("X-Token", $"{token}");
+        request.Content = content;
+
+        var response = await client.SendAsync(request).ConfigureAwait(false);
+        response.EnsureSuccessStatusCode();
+
+
+        if (response.IsSuccessStatusCode)
+        {
+            var resdata = await response.Content.ReadAsStringAsync();
+            dietResult = JsonSerializer.Deserialize<DietResult>(resdata);
+
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                InatializeDiet();
+            });
+        }
     }
 }
---
+++ FlexifyMobile/DietResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexifyMobile
+{
+
+    public class DietResult
+    {
+        public Diet_Json json { get; set; }
+        public bool success { get; set; }
+    }
+
+    public class Diet_Json
+    {
+        public Meal[] breakfast { get; set; }
+        public Meal[] lunch { get; set; }
+        public Meal[] dinner { get; set; }
+        public Meal[] snacks { get; set; }
+    }
+
+    public class Meal
+    {
+        public string name { get; set; }
+        public int calories { get; set; }
+    }
+
+
+}
